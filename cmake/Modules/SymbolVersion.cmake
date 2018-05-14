0b3efd40e0832dc08b5302b155aaf62bcd0b1720
Anderson Toshiyuki Sasaki <ansasaki@redhat.com>
2018-05-14 15:55:06 +0200

    Introduce symbol versioning
    This adds the option to compile with symbol versioning.  The symbol list
    is obtained from the header files by filtering those marked with the
    LIBSSH_API modifier.  Such symbols are used as input to generate the
    version script used by the linker.  The version script is automatically
    updated as new symbols marked with LIBSSH_API are added to the header
    files.  If any symbol is removed, the build will fail due to break in
    the ABI.
    
    Symbol versioning is enabled by default, but disabled in non UNIX like
    platforms. It can be disabled by passing "-DWITH_SYMBOL_VERSIONING=OFF"
    option to cmake or "-withoutsymbolversioning" to "obj/build_make.sh".

---
+++ cmake/Modules/SymbolVersion.cmake
@@ -0,0 +1,59 @@
+# Generate symbol versioning map file (version script)
+#
+# Adds a symver target which gets all symbols marked with the modifier
+# LIBSSH_API in the header files and generate a symbol versioning map file.
+# The smap tool is used to generate and update the map file.
+
+find_package(PythonInterp REQUIRED)
+
+find_program(SMAP smap)
+if (NOT SMAP)
+    message(FATAL_ERROR "Could not find `smap` in PATH."
+                        " It can be found in PyPI as `symver-smap`"
+                        " (try `pip install symver-smap`)")
+endif (NOT SMAP)
+
+find_program(GREP grep)
+if (NOT GREP)
+    message(FATAL_ERROR "Could not find `grep` in PATH.")
+endif (NOT GREP)
+
+find_program(SED sed)
+if (NOT SED)
+    message(FATAL_ERROR "Could not find `sed` in PATH.")
+endif (NOT SED)
+
+find_program(AWK awk)
+if (NOT AWK)
+    message(FATAL_ERROR "Could not find `awk` in PATH.")
+endif (NOT AWK)
+
+if (EXISTS ${CMAKE_CURRENT_SOURCE_DIR}/src/${PROJECT_NAME}.map)
+    set(SMAP_ARGS
+        update -r ${PROJECT_NAME}_${LIBRARY_VERSION} -o
+        ${CMAKE_CURRENT_SOURCE_DIR}/src/${PROJECT_NAME}.map
+        ${CMAKE_CURRENT_SOURCE_DIR}/src/${PROJECT_NAME}.map
+    )
+else ()
+    set(SMAP_ARGS
+        new -r ${PROJECT_NAME}_${LIBRARY_VERSION} -o
+        ${CMAKE_CURRENT_SOURCE_DIR}/src/${PROJECT_NAME}.map
+    )
+endif (EXISTS ${CMAKE_CURRENT_SOURCE_DIR}/src/${PROJECT_NAME}.map)
+
+file (GLOB ALL_HEADERS "${CMAKE_CURRENT_SOURCE_DIR}/include/libssh/*.h")
+
+add_custom_target(symver ALL
+    COMMAND
+      ${GREP} -hri "LIBSSH_API" ${ALL_HEADERS} |
+      ${GREP} -v "\#define" |
+      ${SED} s/.*LIBSSH_API\ // |
+      ${SED} s/\(.*// |
+      ${AWK} {print\ $NF} |
+      ${SED} s/*// |
+      ${SED} s/\ // |
+      ${SMAP} ${SMAP_ARGS}
+    VERBATIM
+    DEPENDS ${ALL_HEADERS}
+    COMMENT "Generating symbol versioning map"
+)
