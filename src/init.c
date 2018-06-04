7fa2057b7689b3a23019fcdeadc92e1abafda6e2
Anderson Toshiyuki Sasaki <ansasaki@redhat.com>
2018-06-04 18:55:25 +0200

    Added auto initialization/finalization functions
    The functions execute the library initialization/finalization when the
    library is loaded/unloaded.  This makes unnecessary to call ssh_init()
    explicitly.

--- src/init.c
+++ src/init.c
@@ -32,11 +32,103 @@
 #include <winsock2.h>
 #endif
 
+# define _CONSTRUCTOR __attribute__((constructor))
+# define _DESTRUCTOR __attribute__((destructor))
+
+#ifdef HAVE_PTHREAD
+
+# include <pthread.h>
+
+#define SSH_STATIC_MUTEX(mutex) \
+    static pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER
+
+# define SSH_STATIC_MUTEX_LOCK(mutex) \
+    pthread_mutex_lock(&mutex)
+
+# define SSH_STATIC_MUTEX_UNLOCK(mutex) \
+    pthread_mutex_unlock(&mutex)
+
+#else /* HAVE_PTHREAD */
+
+# define SSH_STATIC_MUTEX(mutex)
+# define SSH_STATIC_MUTEX_LOCK(mutex)
+# define SSH_STATIC_MUTEX_UNLOCK(mutex)
+
+#endif /* HAVE_PTHREAD */
+
+SSH_STATIC_MUTEX(ssh_init_mutex);
+
+/* Counter for initializations */
+static int _ssh_initialized = 0;
+
+/* Cache the returned value */
+static int _ssh_init_ret = 0;
+
+static int _ssh_init(unsigned constructor) {
+
+    int rc = 0;
+
+    if (!constructor) {
+        SSH_STATIC_MUTEX_LOCK(ssh_init_mutex);
+    }
+
+    _ssh_initialized++;
+
+    if (_ssh_initialized > 1) {
+        rc = _ssh_init_ret;
+        goto _ret;
+    }
+
+    rc = ssh_threads_init();
+    if (rc) {
+        goto _ret;
+    }
+
+    rc = ssh_crypto_init();
+    if (rc) {
+        goto _ret;
+    }
+
+    rc = ssh_socket_init();
+    if (rc) {
+        goto _ret;
+    }
+
+_ret:
+    _ssh_init_ret = rc;
+
+    if (!constructor) {
+        SSH_STATIC_MUTEX_UNLOCK(ssh_init_mutex);
+    }
+
+    return rc;
+}
+
+/**
+ * @brief Initialize global cryptographic data structures.
+ *
+ * This functions is automatically called when the library is loaded.
+ *
+ * @returns             0 on success, -1 if an error occured.
+ */
+static void _CONSTRUCTOR auto_init(void) {
+
+    int rc;
+
+    rc = _ssh_init(1);
+
+    if (rc < 0) {
+        fprintf(stderr, "Error in auto_init()\n");
+    }
+
+    return;
+}
+
 /**
  * @defgroup libssh The libssh API
  *
  * The libssh library is implementing the SSH protocols and some of its
- * extensions. This group of functions is mostly used to implment a SSH client.
+ * extensions. This group of functions is mostly used to implement a SSH client.
  * Some function are needed to implement a SSH server too.
  *
  * @{
@@ -45,39 +137,83 @@
 /**
  * @brief Initialize global cryptographic data structures.
  *
- * This function should only be called once, at the beginning of the program, in
- * the main thread. It may be omitted if your program is not multithreaded.
+ * This function may be ommited if the system supports pthreads.
+ * If the library is already initialized, increments the _ssh_initialized
+ * counter and return the error code cached in _ssh_init_ret.
  *
  * @returns             0 on success, -1 if an error occured.
  */
 int ssh_init(void) {
-  if(ssh_threads_init())
-    return -1;
-  if(ssh_crypto_init())
-    return -1;
-  if(ssh_socket_init())
-    return -1;
-  return 0;
+    return _ssh_init(0);
 }
 
+static int _ssh_finalize(unsigned destructor) {
+
+    if (!destructor) {
+        SSH_STATIC_MUTEX_LOCK(ssh_init_mutex);
+    }
+
+    if (_ssh_initialized == 1) {
+        _ssh_initialized = 0;
+
+        if (_ssh_init_ret < 0) {
+            goto _ret;
+        }
+
+        ssh_crypto_finalize();
+        ssh_socket_cleanup();
+        /* It is important to finalize threading after CRYPTO because
+         * it still depends on it */
+        ssh_threads_finalize();
+
+    }
+    else {
+        if (_ssh_initialized > 0) {
+            _ssh_initialized--;
+        }
+    }
+
+_ret:
+    if (!destructor) {
+        SSH_STATIC_MUTEX_UNLOCK(ssh_init_mutex);
+    }
+    return 0;
+}
+
+/**
+ * @brief Finalize and cleanup all libssh and cryptographic data structures.
+ *
+ * This function is automatically called when the library is unloaded.
+ *
+ * @returns             0 on succes, -1 if an error occured.
+ *
+ */
+static void _DESTRUCTOR auto_finalize(void) {
+
+    int rc;
+
+    rc = _ssh_finalize(1);
+
+    if (rc < 0) {
+        fprintf(stderr, "Error in auto_finalize()\n");
+    }
+
+    return;
+}
 
 /**
  * @brief Finalize and cleanup all libssh and cryptographic data structures.
  *
  * This function should only be called once, at the end of the program!
+ * When called, decrements the counter _ssh_initialized. If the counter reaches
+ * zero, then the libssh and cryptographic data structures are cleaned up.
  *
  * @returns             0 on succes, -1 if an error occured.
  *
-   @returns 0 otherwise
+ @returns 0 otherwise
  */
 int ssh_finalize(void) {
-  ssh_crypto_finalize();
-  ssh_socket_cleanup();
-  /* It is important to finalize threading after CRYPTO because
-   * it still depends on it */
-  ssh_threads_finalize();
-
-  return 0;
+    return _ssh_finalize(0);
 }
 
 /** @} */
